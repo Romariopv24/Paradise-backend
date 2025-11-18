1814ad654e850edcb74b260f3427f6e8498c3ada
Romario Parra <89761966+Romariopv24@users.noreply.github.com>
2025-11-17 20:36:21 -0400

    test

--- src/server.js
+++ src/server.js
@@ -11,45 +11,48 @@ const app = express()
 
 async function expressServer() {
   
-  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swagger_output, {
-    customSiteTitle: "Coally"
-  }))
-  app.use(cookieParser())
-  app.use(express.json())
-  app.use(morgan('dev'))
+ 
+    //crear un middleware
+    app.use(express.json()) // permite procesar cuerpos de solicitudes
+    app.use(morgan('dev')) // muestra peticiones al servidor
 
     const apiPath = {
-      version_api_1: '/api/v1'
-  }
-
-  const originUrls = ['*']
-
-  const corsOptions = {
-        // origin: originUrls,
-        origin: ['http://localhost:5173', '*'],
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        version_api_1: '/api/v1'
     }
 
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin: *');
-      res.header('Access-Control-Allow-Credentials: true');
-      res.header('Access-Control-Allow-Headers', 'Authorization,authorization, X-API-KEY,Access-Control-Allow-Origin,Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-      res.header('Allow', 'GET, POST, OPTIONS');
+    const originUrls = ['*']
 
-      next();
-  });
-
+    const corsOptions = {
+        origin: (origin, callback) => {
+            if(!origin || origenesPermitidos.includes(originUrls)) {
+                callback(null, true)
+            } else { 
+                callback(new Error('Cliente no permitido'))
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    }
 
     app.use(cors(corsOptions))
+    //puerto
+    const PORT = process.env.PORT || 5000
 
-  const PORT = process.env.PORT 
+    //rutas
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin: *');
+        res.header('Access-Control-Allow-Credentials: true');
+        res.header('Access-Control-Allow-Headers', 'Authorization,authorization, X-API-KEY,Access-Control-Allow-Origin,Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.header('Allow', 'GET, POST, OPTIONS');
+        if (req.method === 'OPTIONS') res.sendStatus(200);
+        else next();
+    });
+    app.use(apiPath.version_api_1, routes)
 
-  app.use(apiPath.version_api_1, routes)
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
 
 }
 
